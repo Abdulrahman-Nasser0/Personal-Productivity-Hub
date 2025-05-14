862dcf20b83b4539c98567c92baaff8a59ff06a7
Abdulrahman Nasser <abdulrahman.nasser222@gmail.com>
2025-05-14 15:12:08 +0300

    Added Docker and requirement files

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ migrate = Migrate()
 def create_app():
     app = Flask(__name__)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'app.db')
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
     app.config['SECRET_KEY'] = 'xyz' 
    
     db.init_app(app)
